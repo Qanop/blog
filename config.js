c0207699214f83ebd5e9bb23f043af0e6cb62f57
Przemysław Sawczuk <przemeksawczuk@yahoo.pl>
2020-10-26 23:13:41 +0100

    init

---
+++ config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  url: 'https://blog.sawczuk.app',
+  pathPrefix: '/',
+  title: 'Just another fancy blog.',
+  subtitle: 'The beginning is never easy. But we can get used to the difficulties and add a baggage of experience along the way.',
+  copyright: '© All rights reserved.',
+  disqusShortname: 'atryni_blog',
+  postsPerPage: 6,
+  googleAnalyticsId: '0',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'Categories',
+      path: '/categories'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    }
+  ],
+  author: {
+    name: 'Przemysław Sawczuk',
+    photo: '/photo.jpg',
+    bio: 'The beginning is never easy. But we can get used to the difficulties and add a baggage of experience along the way.',
+    contacts: {
+      email: 'przemeksawczuk@yahoo.pl',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'atryni',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: 'atryni_',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      flickr: 'atryniphotography',
+      www: 'https://sawczuk.app/'
+    }
+  }
+};
---
+++ content/posts/2020-09-01---reboot-your-raspberry-pi.md
@@ -0,0 +1,15 @@
+---
+title: Reboot your Raspberry Pi
+date: "2020-09-01T21:00:00.000Z"
+template: "post"
+draft: false
+slug: "reboot-your-raspberry-pi"
+category: "Technology"
+tags:
+  - "Raspberry Pi"
+  - "Server"
+description: ""
+socialImage: "media/lights.jpg"
+---
+
+abc
