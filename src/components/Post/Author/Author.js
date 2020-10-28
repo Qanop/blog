eace79e625125e1877905f868705b9e36b5e396c
Przemysław Sawczuk <przemeksawczuk@yahoo.pl>
2020-10-28 02:47:22 +0100

    Updated yarn-lock. Changed author data

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,6 +1,6 @@
 // @flow strict
 import React from 'react';
-import { getContactHref } from '../../../utils';
+// import { getContactHref } from '../../../utils';
 import styles from './Author.module.scss';
 import { useSiteMetadata } from '../../../hooks';
 
@@ -11,14 +11,8 @@ const Author = () => {
     <div className={styles['author']}>
       <p className={styles['author__bio']}>
         {author.bio}
-        <a
-          className={styles['author__bio-twitter']}
-          // href={getContactHref('twitter', author.contacts.twitter)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <strong>{author.name}</strong>
-        </a>
+        <br/>
+        <strong><u>{author.name}</u></strong>
       </p>
     </div>
   );
